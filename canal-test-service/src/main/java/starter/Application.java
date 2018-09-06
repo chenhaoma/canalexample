4d6dd9b1f0e4fae8daa63edd486644d2bd63cef2
chenhao.ma <chenhao.ma@weimob.com>
2018-09-06 15:52:15 +0800

    init

---
+++ canal-test-service/src/main/java/starter/Application.java
@@ -0,0 +1,14 @@
+package starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by MCH on 2018/9/6.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
